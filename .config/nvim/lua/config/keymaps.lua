9f24f902244c3971afe03a464fffb3de788c0e7b
Joe Roth <joe.roth.95@web.de>
2024-09-06 15:17:28 +0200

    added keymaps, options and neotree plugin'

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+-- Telescope Symbols
+vim.keymap.set("n", "<leader>fs", "<cmd> Telescope symbols<cr>", { desc = "Find Symbols/Smileys" })
+
+-- toggle Neotree on right side
+vim.keymap.set("n", "<C-n>", ":Neotree toggle filesystem reveal right<CR>", { desc = "toggle Neotree on right side" })
