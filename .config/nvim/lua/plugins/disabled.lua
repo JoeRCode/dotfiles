03864df7254bce596b9c3e1e4c6bde5992d46c44
Joe Roth <joe.roth.95@web.de>
2024-09-20 01:14:48 +0200

    disabled indent blankline for more overview

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
 }
