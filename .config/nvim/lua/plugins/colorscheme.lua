f23cc59b075b5ea286573f0f5afc0daea8e7887b
Joe Roth <joe.roth.95@web.de>
2024-09-20 02:23:28 +0200

    changed back to carbonfox

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
--- bg color is #262626 (for terminal)
+-- bg color is #262626 (for nightfox terminal)
+-- bg color is #121212 (for carbon terminal)
 return {
   { "EdenEast/nightfox.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfox",
+      colorscheme = "carbonfox",
     },
   },
 }
