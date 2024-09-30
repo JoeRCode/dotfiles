d3eb09418b020f084c367aaeb01c44e2db279828
Joe Roth <joe.roth.95@web.de>
2024-09-30 18:07:38 +0200

    added comments

--- Scripts/setdpi.sh
+++ Scripts/setdpi.sh
@@ -5,7 +5,8 @@
 # ratbagctl device name changes after loading a new mouse profile (MX Master 3)
 
 # get the device name
-DEVICE_NAME=$(ratbagctl list | cut -f1 -d:)
+# -f1 = first field | -d: = set the delimeter to ':'
+DEVICE_NAME=$(ratbagctl list | cut -f1 -d ':')
 
 if [[ $1 -eq 0 ]]; then
   ratbagctl $DEVICE_NAME dpi set 400
